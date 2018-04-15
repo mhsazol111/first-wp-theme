0191081d05518947c7737cd3d9b55cad5808d46f
SAZOL <mhsazol111@gmail.com>
2018-04-15 09:25:13 +0600

    addmin Admin Template

--- core-files/admin-functions.php
+++ core-files/admin-functions.php
@@ -7,11 +7,21 @@
 */
 
 function itw_add_admin_panel() {
-	add_menu_page( 'iTiger Theme Options', 'iTiger', 'manage_options', 'itw-theme-options', 'itw_create_theme_page', get_template_directory_uri() . '/assets/img/itw-theme-icon.png', 120 );
+	// Adding Theme Options Page
+	add_menu_page( 'iTiger Theme Options', 'iTiger', 'manage_options', 'itw_theme_options', 'itw_create_theme_page', get_template_directory_uri() . '/assets/img/itw-theme-icon.png', 120 );
+	// Adding Settings Page
+	add_submenu_page( 'itw_theme_options', 'General Options', 'General', 'manage_options', 'itw_theme_options', 'itw_create_theme_page' );
+	//Adding Header Options
+	add_submenu_page( 'itw_theme_options', 'Header Options', 'Header', 'manage_options', 'itw_header_options', 'itw_header_options' );
 }
 add_action( 'admin_menu', 'itw_add_admin_panel' );
 
 
 function itw_create_theme_page() {
 	// Call back generation Function
+	require_once ( get_template_directory() . '/core-files/admin-templates/general-admin-page.php' );
+}
+
+function itw_header_options() {
+	// Call back header function
 }
---
+++ core-files/admin-templates/general-admin-page.php
@@ -0,0 +1,1 @@
+<h1>General Admin Page</h1>
